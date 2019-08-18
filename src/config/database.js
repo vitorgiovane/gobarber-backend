4bdbe725f5cd1afefb966da1be17e7c8ed3f50f5
Vitor <vitorgpalves@gmail.com>
2019-08-18 19:54:17 -0300

    feature: configura Sequelize
    ===
    aula 07

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+  dialect: 'postgres',
+  host: 'localhost',
+  database: 'gobarber',
+  usename: 'gobarber',
+  password: 'password',
+  define: {
+    timestamp: true,
+    underscored: true,
+    underscoredAll: true
+  }
+}
