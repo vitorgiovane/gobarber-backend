c55bd5e41b3a9e1839d929b2de9bdfcaf5696c95
Vitor <vitorgpalves@gmail.com>
2019-08-19 01:04:09 -0300

    feature: cria loader de models
    ===
    aula 10

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,14 @@
 import { Router } from 'express'
+import User from './app/models/User'
 
 const routes = Router()
-routes.get('/', (req, res) => {
-  return res.json({ message: 'Hello, World!!!' })
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Vitor Giovane',
+    email: 'vitorgpalves@gmail.com',
+    password: '123456'
+  })
+  return res.json(user)
 })
 
 export default routes
