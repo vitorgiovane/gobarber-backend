37828aa3a739793038857dc7e52358a391c11481
Vitor Giovane <vitor.giovane@commcepta.com.br>
2019-10-29 11:09:26 -0300

    feature: adiciona suporte a variáveis de ambiente

--- src/config/database.js
+++ src/config/database.js
@@ -1,10 +1,12 @@
+require('dotenv').config()
+
 module.exports = {
-  dialect: 'postgres',
-  host: 'localhost',
-  port: 2030,
-  username: 'gobarber',
-  password: 'password',
-  database: 'gobarber',
+  dialect: process.env.DB_DIALECT,
+  host: process.env.DB_HOST,
+  port: process.env.DB_EXTERNAL_PORT,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   define: {
     timestamps: true,
     underscored: true,
