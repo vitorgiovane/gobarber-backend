fe452dafb34b7fe04c174e2be7294dbd16f11205
Vitor Giovane <vitor.giovane@commcepta.com.br>
2019-10-29 14:26:58 -0300

    feature: valida dados de entrada
    ===
    aula 17

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,7 +1,18 @@
+import * as Yup from "yup"
 import User from './../models/User'
 
 class UserController {
   async store(request, response) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      email: Yup.string().email().required(),
+      password: Yup.string().required().min(6)
+    })
+
+    if(!await schema.isValid(request.body)){
+      return response.status(400).json({error: "Validation fails"})
+    }
+
     const userExists = await User.findOne({
       where: { email: request.body.email }
     })
@@ -15,6 +26,22 @@ class UserController {
   }
 
   async update(request, response){
+    const schema = Yup.object().shape({
+      name: Yup.string(),
+      email: Yup.string().email(),
+      password: Yup.string().min(6),
+      oldPassword: Yup.string().min(6).when("password", (password, oldPassword) =>
+        password ? oldPassword.required() : oldPassword
+      ),
+      passwordConfirmation: Yup.string().when("password", (password, field) =>
+        password ? field.required().oneOf([Yup.ref("password")]) : field
+      )
+    })
+
+    if (!await schema.isValid(request.body)) {
+      return response.status(400).json({ error: "Validation fails" })
+    }
+
     const { email, oldPassword } = request.body
     const user = await User.findByPk(request.userId)
 
